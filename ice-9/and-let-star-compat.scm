fedc330e463d052dfedaebc2638b1f5b3e6432d3
Thien-Thi Nguyen <ttn@gnuvola.org>
2002-04-08 20:45:15 +0000

    Initial revision

---
+++ ice-9/and-let-star-compat.scm
@@ -0,0 +1,12 @@
+;;;; This file will be installed as "and-let*.scm" on systems that
+;;;; support it.  It will go away in the future, use the module
+;;;; (ice-9 and-let-star) instead.
+
+(define-module (ice-9 and-let*)
+  :use-module (ice-9 and-let-star)
+  :re-export-syntax (and-let*))
+
+(display ";;; The module name (ice-9 and-let*) is deprecated.\n"
+	 (current-error-port))
+(display ";;; Use (ice-9 and-let-star) instead.\n\n"
+	 (current-error-port))
