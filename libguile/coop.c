7d81830dd2383e9eeafa3885497264017886178c
Marius Vollmer <mvo@zagadka.de>
2005-05-23 20:15:36 +0000

    The FSF has a new address.

--- libguile/coop.c
+++ libguile/coop.c
@@ -12,8 +12,8 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
  *
  * As a special exception, the Free Software Foundation gives permission
  * for additional uses of the text contained in its release of GUILE.
@@ -40,7 +40,7 @@
  * If you do not wish that, delete this exception notice.  */
 
 
-/* $Id: coop.c,v 1.27.4.1 2001-10-06 16:28:34 mdj Exp $ */
+/* $Id: coop.c,v 1.27.4.2 2005-05-23 20:15:32 mvo Exp $ */
 
 /* Cooperative thread library, based on QuickThreads */
 
