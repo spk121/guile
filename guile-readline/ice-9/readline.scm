35a69665b8a0cf63b2c0e425be41a426ddef4dfe
Kevin Ryde <user42@zip.com.au>
2006-10-04 22:31:00 +0000

    (filename-completion-function): Export this.

--- guile-readline/ice-9/readline.scm
+++ guile-readline/ice-9/readline.scm
@@ -1,6 +1,6 @@
 ;;;; readline.scm --- support functions for command-line editing
 ;;;;
-;;;; 	Copyright (C) 1997, 1999, 2000, 2001, 2002, 2005 Free Software Foundation, Inc.
+;;;; 	Copyright (C) 1997, 1999, 2000, 2001, 2002, 2005, 2006 Free Software Foundation, Inc.
 ;;;; 
 ;;;; This program is free software; you can redistribute it and/or modify
 ;;;; it under the terms of the GNU General Public License as published by
@@ -27,7 +27,8 @@
   :use-module (ice-9 session)
   :use-module (ice-9 regex)
   :use-module (ice-9 buffered-input)
-  :no-backtrace)
+  :no-backtrace
+  :export (filename-completion-function))
 
 
 
