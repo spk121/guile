0d473e03c05402bf50e973199ca2970e0c408c3b
Neil Jerram <neil@ossau.uklinux.net>
2010-03-05 22:55:14 +0000

    Patch ltmain.sh to handle args correctly when invoking a Windows executable
    (From http://git.savannah.gnu.org/cgit/libtool.git/commit/?id=101ad44541c6d303cf465937a212042885f4338e)
    
    * ltmain.sh.patch: New file.
    
    * autogen.sh: Apply patch to build-aux/ltmain.sh.

--- autogen.sh
+++ autogen.sh
@@ -13,4 +13,10 @@ set -e
 
 autoreconf -i --force --verbose
 
+# Patch build-aux/ltmain.sh to apply fix for handling arguments
+# correctly when invoking Windows executables.  This patch comes from
+# libtool's Git repository:
+# http://git.savannah.gnu.org/cgit/libtool.git/commit/?id=101ad44541c6d303cf465937a212042885f4338e
+patch -p1 < ltmain.sh.patch
+
 echo "Now run configure and make."
