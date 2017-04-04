8666a34d5e8ec4e22155b40f55e53f3a652b96f2
Michael Gran <spk121@yahoo.com>
2017-04-04 08:10:03 -0700

    Disable scm_fork on Cygwin
    Cygwin's fork is incompatible with Guile's thread and GC model
    
    * libguile/posix.c (HAVE_FORK) [__CYGWIN__]: #undef HAVE_FORK when __CYGWIN__

--- libguile/posix.c
+++ libguile/posix.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
  *   2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013,
- *   2014, 2016 Free Software Foundation, Inc.
+ *   2014, 2016, 2017 Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -83,6 +83,12 @@
 
 #ifdef __MINGW32__
 # include "posix-w32.h"
+#endif
+
+/* Note that Cygwin's DLL 2.8.0's forking model is incompatible with
+   Guile.  Forking is disabled on Cygwin.  */
+#ifdef __CYGWIN__
+# undef HAVE_FORK
 #endif
 
 #if HAVE_SYS_WAIT_H
