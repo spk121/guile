4686d4b8de1ae17d32764d14875577e7fa2c4dc3
Michael Gran <spk121@yahoo.com>
2020-12-22 08:32:55 -0800

    Avoid falling back to ComSpec when execp fails on Windows
    When the start_child procedure fails to launch a procedure,
    described as an argv, it tries again, using ComSpec as a shell
    and argv as command line arguments. However, this 2nd attempt
    if it fails, will spawn an interactive shell waiting for input.
    
    This 2nd attempt would not have been reached if the argv were a proper
    batch file or executable, so it only exists to run commands built
    in to the ComSpec interpreter.
    
    * libguile/posix-w32.c (start_child): remove 2nd attempt

--- libguile/posix-w32.c
+++ libguile/posix-w32.c
@@ -740,22 +740,6 @@ start_child (const char *exec_file, char **argv,
   CloseHandle (herr);
   CloseHandle (pi.hThread);
 
-  /* Posix requires to call the shell if execvp fails to invoke EXEC_FILE.  */
-  if (errno_save == ENOEXEC || errno_save == ENOENT)
-    {
-      const char *shell = getenv ("ComSpec");
-
-      if (!shell)
-        shell = "cmd.exe";
-
-      if (c_strcasecmp (exec_file, shell) != 0)
-        {
-          argv[0] = (char *)exec_file;
-          return start_child (shell, argv, reading, c2p, writing, p2c,
-                              infd, outfd, errfd);
-        }
-    }
-
   errno = errno_save;
   return pid;
 }
