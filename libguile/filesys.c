df8e772f3c5f8c998b074a2821320544da8fb088
Mike Gran <spk121@yahoo.com>
2022-09-15 03:55:17 -0700

    Presume time.h and sys/time.h don't conflict when included
    Systems on which time.h and sys/time.h conflicted are obsolescent.
    
    * configure.ac: remove AC_HEADER_TIME. remove conditional
      in tm.tm_gmtoff test.
    * libguile/filesys.c [TIME_WITH_SYS_TIME]: remove conditional
    * libguile/posix.c [TIME_WITH_SYS_TIME]: remove conditional
    
    # Conflicts:
    #	libguile/filesys.c

--- libguile/filesys.c
+++ libguile/filesys.c
@@ -53,16 +53,10 @@
 #include <io.h>
 #endif
 
-#ifdef TIME_WITH_SYS_TIME
+#if HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
+#include <time.h>
 
 #ifdef LIBC_H_WITH_UNISTD_H
 #include <libc.h>
