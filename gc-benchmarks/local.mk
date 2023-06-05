b14ce93fa9fe48f3052286ff9dfa27f0b64db32b
Ludovic Courtès <ludo@gnu.org>
2023-06-05 10:26:07 +0200

    Remove recursive Makefile for 'gc-benchmarks'.
    * gc-benchmarks/Makefile.am: Rename to...
    * gc-benchmarks/local.mk: ... this.  Prefix file names with %D%.
    (benchmarks): Rename to...
    (gc_benchmarks): ... this.
    * Makefile.am: Include it.
    * configure.ac: Don't output 'gc-benchmarks/Makefile'.

--- gc-benchmarks/local.mk
+++ gc-benchmarks/local.mk
@@ -1,6 +1,6 @@
 ## Process this file with automake to produce Makefile.in.
 ##
-##  	Copyright (C) 2011 Free Software Foundation, Inc.
+##  	Copyright (C) 2011, 2023 Free Software Foundation, Inc.
 ##
 ##   This file is part of GUILE.
 ##
@@ -20,36 +20,36 @@
 ##   Fifth Floor, Boston, MA 02110-1301 USA
 
 EXTRA_DIST =					\
-  gc-profile.scm				\
-  gcbench.scm					\
-  guile-test.scm				\
-  loop.scm					\
-  run-benchmark.scm				\
-  string.scm					\
-  $(benchmarks)
+  %D%/gc-profile.scm				\
+  %D%/gcbench.scm				\
+  %D%/guile-test.scm				\
+  %D%/loop.scm					\
+  %D%/run-benchmark.scm				\
+  %D%/string.scm				\
+  $(gc_benchmarks)
 
 # GPLv2+ Larceny GC benchmarks by Lars Hansen et al. from
 # <http://www.ccs.neu.edu/home/will/GC/sourcecode.html>.
-benchmarks =					\
-  larceny/GPL					\
-  larceny/README				\
-  larceny/dumb.sch				\
-  larceny/dummy.sch				\
-  larceny/dynamic-input-large.sch		\
-  larceny/dynamic-input-small.sch		\
-  larceny/dynamic.sch				\
-  larceny/earley.sch				\
-  larceny/gcbench.sch				\
-  larceny/gcold.scm				\
-  larceny/graphs.sch				\
-  larceny/lattice.sch				\
-  larceny/nboyer.sch				\
-  larceny/nucleic2.sch				\
-  larceny/perm.sch				\
-  larceny/run-benchmark.chez			\
-  larceny/sboyer.sch				\
-  larceny/softscheme.sch			\
-  larceny/twobit-input-long.sch			\
-  larceny/twobit-input-short.sch		\
-  larceny/twobit-smaller.sch			\
-  larceny/twobit.sch
+gc_benchmarks =					\
+  %D%/larceny/GPL				\
+  %D%/larceny/README				\
+  %D%/larceny/dumb.sch				\
+  %D%/larceny/dummy.sch				\
+  %D%/larceny/dynamic-input-large.sch		\
+  %D%/larceny/dynamic-input-small.sch		\
+  %D%/larceny/dynamic.sch			\
+  %D%/larceny/earley.sch			\
+  %D%/larceny/gcbench.sch			\
+  %D%/larceny/gcold.scm				\
+  %D%/larceny/graphs.sch			\
+  %D%/larceny/lattice.sch			\
+  %D%/larceny/nboyer.sch			\
+  %D%/larceny/nucleic2.sch			\
+  %D%/larceny/perm.sch				\
+  %D%/larceny/run-benchmark.chez		\
+  %D%/larceny/sboyer.sch			\
+  %D%/larceny/softscheme.sch			\
+  %D%/larceny/twobit-input-long.sch		\
+  %D%/larceny/twobit-input-short.sch		\
+  %D%/larceny/twobit-smaller.sch		\
+  %D%/larceny/twobit.sch
