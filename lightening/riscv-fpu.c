2a90f632552f566ece90218c29f3e07a3fffe33e
Ekaitz Zarraga <ekaitz@elenq.tech>
2025-12-09 19:53:55 +0100

    Add missing license headers.
    * .woodpecker.yml, lightening/riscv-fpu.c: Add license header.

--- lightening/riscv-fpu.c
+++ lightening/riscv-fpu.c
@@ -1,4 +1,24 @@
 /*
+ * Copyright (C) 2012-2024  Free Software Foundation, Inc.
+ *
+ * This file is part of GNU lightning.
+ *
+ * GNU lightning is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * GNU lightning is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Authors:
+ *      Paulo Cesar Pereira de Andrade
+ *      Ekaitz Zarraga <ekaitz@elenq.tech>
+ */
+
+/*
  * RV32F Standard Extension
  */
 #define _FLW(rd, rs1, im)              Itype(7, rd, 2, rs1, im)
