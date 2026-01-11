spk121/guile-jonas

commit 137b0e85b956a7b7e7c9bc0029d09fe6868e5b17

(system base target) doesn't load (system foreign)
* module/system/base/target.scm (%native-word-size): Use
most-positive-fixnum instead of importing (system foreign).

module/system/base/target.scm
@@ -1,6 +1,6 @@
 ;;; Compilation targets
 
-;; Copyright (C) 2011-2014,2017-2018,2023 Free Software Foundation, Inc.
+;; Copyright (C) 2011-2014,2017-2018,2023-2024 Free Software Foundation, Inc.
 
 ;; This library is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public
@@ -44,8 +44,16 @@
 ;;; Target types
 ;;;
 
-(define %native-word-size
-  ((@ (system foreign) sizeof) '*))
+;; Hacky way to get native pointer size without having to load (system
+;; foreign).
+(define-syntax %native-word-size
+  (lambda (stx)
+    (syntax-case stx ()
+      (id (identifier? #'id)
+          (cond
+           ((< most-positive-fixnum (ash 1 32)) 4)
+           ((< most-positive-fixnum (ash 1 64)) 8)
+           (else (error "unexpected!" most-positive-fixnum)))))))
 
 (define %target-type (make-fluid %host-type))
 (define %target-endianness (make-fluid (native-endianness)))

spk121/guile

commit 3d2b8f827f68b15a2602bb7834ce9f0f7beca429

target: Get native word size right when cross-compiled.
Fixes a regression introduced in
137b0e85b956a7b7e7c9bc0029d09fe6868e5b17.

* module/system/base/target.scm (%native-word-size): Turn into a
variable.
* NEWS: Update.

Fixes: guile/guile#88
Reported-by: Yelninei <yelninei@tutamail.com>
Signed-off-by: Ludovic Courtès <ludo@gnu.org>
Merges: #89

module/system/base/target.scm
@@ -45,15 +45,13 @@
 ;;;
 
 ;; Hacky way to get native pointer size without having to load (system
-;; foreign).
-(define-syntax %native-word-size
-  (lambda (stx)
-    (syntax-case stx ()
-      (id (identifier? #'id)
-          (cond
-           ((< most-positive-fixnum (ash 1 32)) 4)
-           ((< most-positive-fixnum (ash 1 64)) 8)
-           (else (error "unexpected!" most-positive-fixnum)))))))
+;; foreign).  Note: Use the value of 'most-positive-fixnum' available at
+;; run time, not a macro-expansion time, to ensure the correct value is
+;; used when cross-compiling.
+(define %native-word-size
+  (cond ((< most-positive-fixnum (ash 1 32)) 4)
+        ((< most-positive-fixnum (ash 1 64)) 8)
+        (else (error "unexpected!" most-positive-fixnum))))
 
 (define %target-type (make-fluid %host-type))
 (define %target-endianness (make-fluid (native-endianness)))