ecf34ae5c1920e8d20ec29e8838ea19c6737b467
Zhu Zihao <all_but_last@163.com>
2026-01-27 01:13:57 +0800

    Update Unicode Data to 17.0.0.
    * libguile/UnicodeData.txt: Update to 17.0.0.
    * module/scheme/char.scm (*decimal-zeroes*): Ditto.
    * NEWS: Mention Unicode Data update.

--- module/scheme/char.scm
+++ module/scheme/char.scm
@@ -54,17 +54,18 @@
 
 ;; The table can be extracted with:
 ;; awk -F ';' '/ZERO;Nd/ {print "#x"$1}' UnicodeData.txt
-;; Up to date with Unicode 15.1.0
+;; Up to date with Unicode 17.0.0.
 
 (define *decimal-zeroes* '#(#x0030 #x0660 #x06F0 #x07C0 #x0966 #x09E6
   #x0A66 #x0AE6 #x0B66 #x0BE6 #x0C66 #x0CE6 #x0D66 #x0DE6 #x0E50
   #x0ED0 #x0F20 #x1040 #x1090 #x17E0 #x1810 #x1946 #x19D0 #x1A80
   #x1A90 #x1B50 #x1BB0 #x1C40 #x1C50 #xA620 #xA8D0 #xA900 #xA9D0
-  #xA9F0 #xAA50 #xABF0 #xFF10 #x104A0 #x10D30 #x11066 #x110F0 #x11136
-  #x111D0 #x112F0 #x11450 #x114D0 #x11650 #x116C0 #x11730 #x118E0
-  #x11950 #x11C50 #x11D50 #x11DA0 #x11F50 #x16A60 #x16AC0 #x16B50
+  #xA9F0 #xAA50 #xABF0 #xFF10 #x104A0 #x10D30 #x10D40 #x11066 #x110F0
+  #x11136 #x111D0 #x112F0 #x11450 #x114D0 #x11650 #x116C0 #x116D0
+  #x116DA #x11730 #x118E0 #x11950 #x11BF0 #x11C50 #x11D50 #x11DA0
+  #x11DE0 #x11F50 #x16130 #x16A60 #x16AC0 #x16B50 #x16D70 #x1CCF0
   #x1D7CE #x1D7D8 #x1D7E2 #x1D7EC #x1D7F6 #x1E140 #x1E2F0 #x1E4F0
-  #x1E950 #x1FBF0))
+  #x1E5F1 #x1E950 #x1FBF0))
 
 (define (digit-value char)
   (define (cmp zero ch)
